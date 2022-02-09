c2ef89550e175887ff5fda4d1d1fdc056a5f9e57
Gerald Montero <geraldmontero12@gmail.com>
2022-02-09 09:19:15 -0600

    warm up

---
+++ js/warmupfeb9.js
@@ -0,0 +1,17 @@
+// write a function that takes a string as argument and returns the number of vowels contained in that string.
+//     The vowels are .
+// input: "helicopter"
+// output: 4
+// reason: there are 4 vowels in the input word helicopter
+
+// let vowels = [ 'a, e, i, o, u' ];
+// function countVowels(subject) {
+//     console.log( subject.match(/[aeiou]/gi).length)
+// }
+
+let string = helicopter
+function getVowels(string) {
+    var m = str.match(/[aeiou]/gi);
+    return m === null ? 0 : m.length;
+    console.log(m)
+}
